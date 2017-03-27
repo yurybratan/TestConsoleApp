c58b09dbe36f22231f7af2d3e05787e5df032290
Orest <orest.khrapko@cardworks.com>
2017-03-27 16:23:12 -0400

    Commit 1

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace TestConsole {
     class Program {
         static void Main(string[] args) {
+            Console.WriteLine("Hello Git");
         }
     }
 }
