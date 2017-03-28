8e1d3fb27095dfcc0bcdd61fcff93be55cb2f9f4
Eugene <eugene.blum@gmail.com>
2017-03-27 22:52:36 -0400

    -m "eugene"

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace TestConsole {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello Git 2");
+            Console.WriteLine("Hello Eugene");
         }
     }
 }
